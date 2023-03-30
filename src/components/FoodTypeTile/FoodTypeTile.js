158773d0eec460c9ba941b736f1b67b9f3c8daf0
PeminduRayan <pemirayan@gmail.com>
2023-03-30 16:58:39 +0530

    One compo completed

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -4,8 +4,12 @@ import breakfast_img from '../../assets/breakfast.png';
   
 const FoodTypeTile = () => {
     return ( 
-        <div>
-            <img src= {breakfast_img}/>   
+        <div className={classes.outer} >
+            <img className={classes.img} src= {breakfast_img}/>   
+                <div className={classes.divboth}>
+                    <span className={classes.mainTitle}>Breakfast</span>
+                    <span className={classes.subTitle}>13 items</span>
+                </div>
         </div>
      );
 }
