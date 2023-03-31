146d58f49ccf9035108cce92444308350885b2c1
PeminduRayan <pemirayan@gmail.com>
2023-03-31 14:43:51 +0530

    Images Added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import FoodTypeTile from './components/FoodTypeTile/FoodTypeTile';
 import FoodTileContainer from './components/FoodTypeTile/FoodTileContainer';
+import MainLogo from './components/NavBar/MainLogo';
 
 
 function App() {
   return (
     <div className="App">
+      <MainLogo/>
         <FoodTileContainer/>
     </div>
   );
